dc6cfc95fa7e9e857c1f17f52954d28dc9828c65
Joel Healy <joel.h.healy@gmail.com>
2017-12-18 12:52:13 -0600

    In class Hello World and Class 1 Prep Exercise 1 completed

---
+++ Class01/Class01PrepExercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  
+ *  LaunchCode 101 - Unit 3: CSharp
+ *  
+ *  Class 1 - Prep Exercise 1
+ *  
+ *  Input/output: Modify your HelloWorld program to prompt the user for
+ *  their name, and greet them by name.using System;
+ *  
+ */
+
+using System;
+
+namespace Class01PrepExercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello, " + username + "!");
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
